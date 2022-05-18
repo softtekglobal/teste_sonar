71a841a4e30c39903991b67c7ffb633ecae0dc7b
Fabio Paiva <guaravitonrj@gmail.com>
2022-05-18 16:44:45 -0300

    teste

--- src/main/java/com/softtek/testesonar/demo/TesteRepository.java
+++ src/main/java/com/softtek/testesonar/demo/TesteRepository.java
@@ -69,6 +69,27 @@ public class TesteRepository {
 		return createNativeQuery(sql.toString(), parameters,GraficoContratoPedidosDTO.class);
 	}
 	
+	public boolean authenticate(javax.servlet.http.HttpServletRequest request, java.sql.Connection connection) throws SQLException {
+		  String user = request.getParameter("user");
+		  String pass = request.getParameter("pass");
+
+		  String query = "SELECT * FROM users WHERE user = '" + user + "' AND pass = '" + pass + "'"; // Unsafe
+
+		  // If the special value "foo' OR 1=1 --" is passed as either the user or pass, authentication is bypassed
+		  // Indeed, if it is passed as a user, the query becomes:
+		  // SELECT * FROM users WHERE user = 'foo' OR 1=1 --' AND pass = '...'
+		  // As '--' is the comment till end of line syntax in SQL, this is equivalent to:
+		  // SELECT * FROM users WHERE user = 'foo' OR 1=1
+		  // which is equivalent to:
+		  // SELECT * FROM users WHERE 1=1
+		  // which is equivalent to:
+		  // SELECT * FROM users
+
+		  java.sql.Statement statement = connection.createStatement();
+		  java.sql.ResultSet resultSet = statement.executeQuery(query); // Noncompliant
+		  return resultSet.next();
+		}
+	
 	@SuppressWarnings({ "deprecation", "rawtypes" })
 	private List createNativeQuery(String sql, Map<String, String> parameters, Class target) {
 		Query query = getCurrentSession().createNativeQuery(sql);
